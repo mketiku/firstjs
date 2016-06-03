a3b282ae9735dab8da286b6d4a863526436cd526
mketiku <mketiku@gmail.com>
2016-06-03 09:32:01 -0400

    Push to Github

---
+++ day1.js
@@ -0,0 +1,9 @@
+for (var i = 1); (i < 100) ; (i ++){
+    if i % 15 == 0 then
+   console.log("fizzbuz" + i)
+    else  if i % 3 = = 0  then
+    console.log("fizz" + i)
+    else if i % 5 == 0 then
+    console.log("buzz" + iw
+}
+f
---
+++ js/app.js
@@ -0,0 +1,40 @@
+$(document).foundation()
+
+function addValuesToDetails() {
+  this
+}
+
+var buildColorDiv = function() {
+
+};
+
+var addValuesToDetails = function(ev) {
+  ev.preventDefault();
+  var details = document.querySelector('div.details');
+
+  var firstName = this.firstName.value;
+  var hairColor = this.hairColor.value;
+  var age = this.age.value;
+  var birthplace = this.birthplace.value;
+
+  var colorDiv = '<div style="height: 100px; width: 100px; background-color: ' + hairColor + '"></div>';
+
+  details.innerHTML += ' \
+    <dl> \
+      <dt>First Name</dt> \
+      <dd>' + firstName + '</dd> \
+        \
+      <dt>Hair Color</dt> \
+      <dd>' + colorDiv + '</dd> \
+        \
+      <dt>Age</dt> \
+      <dd>' + age + '</dd> \
+        \
+      <dt>Birthplace</dt> \
+      <dd>' + birthplace + '</dd> \
+        \
+    </dl>';
+};
+
+var myForm = document.querySelector('form');
+myForm.onsubmit = addValuesToDetails;
